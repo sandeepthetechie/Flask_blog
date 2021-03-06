ad25ee3eb560d702d18886a73cd2bc02fc10073b
sandeepthetechie <sandeeps808@gmail.com>
2021-03-06 22:46:06 +0530

    10pm

--- flask_blog.py
+++ flask_blog.py
@@ -4,6 +4,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    username = db.Column(db.String(120), unique=True, nullable= False)
+    image_file = db.Column(db.String(20), nullable=False, default = 'default.jpg')
+    password = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    
+
+
 
 posts = [
     {
